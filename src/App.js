afec3ea3ae857d14c93229a80877f060ad1a9828
mahmood-alashqar <mahmood.ashqur@hotmail.com>
2021-04-27 15:35:49 +0300

    lab02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+  import React from'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Main from './component/main';
+import Header from './component/header';
+import Footer from './component/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  render()
+  {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer/>
+      </div>
+    )
+
+  };
 }
 
 export default App;
---
+++ src/component/HornedBeasts.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+class HornedBeasts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state =
+          { count: 0, };
+      }
+      increaseCount = () => {
+        this.setState(
+          {
+            count: this.state.count + 1
+          }
+    
+        );
+      }
+    render()
+    {
+return (
+    <div>
+
+ <Card border="primary" style={{ width: '18rem' }}>
+          <Card.Header> {this.props.title}</Card.Header>
+          <Card.Body>
+            <Card.Title> {this.props.title}</Card.Title>
+            {/* <Card.Img>onClick= {this.increaseCount} src={this.props.img} alt="the img"</Card.Img> */}
+            <Card.Img onClick={this.increaseCount} src={this.props.img} />
+            <Card.Text>
+              {this.props.disc}
+            </Card.Text>
+            <Card.Text>
+              💖 {this.state.count}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+      </div>
+    );
+  }
+}
+export default HornedBeasts;
