8926f27f9b1c53a33386396356dbf96afb267350
mahmood-alashqar <mahmood.ashqur@hotmail.com>
2021-04-29 20:53:20 +0300

    lab03

--- src/component/model.js
+++ src/component/model.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 class Model extends React.Component {
 
